ba181d2a112e701cadf6f63fa26ad9f6dc164b6c
Yugesh <242237@slcs.edu.in>
2025-07-03 13:14:54 +0530

    datasetsss!!

---
+++ datasets/sql 242217/SQLQuery2.sql
@@ -0,0 +1,9 @@
+use studentdetailssss
+create table student_data
+(rollno INT ,stname VARCHAR(255),age INT,place VARCHAR(255));
+insert into student_data(rollno,stname,age,place)
+values(242217,'balahari',18, 'mdu'),
+(242218,'chandru',18,'mdu'),
+(242219,'arul',18,'apk')
+select *from student_data;
+
